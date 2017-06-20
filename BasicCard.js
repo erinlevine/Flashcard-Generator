cd7aeab9eb1fc179eeaaa99545b0cd4c550c1131
erinlevine <erinclevine@gmail.com>
2017-06-20 15:40:19 -0400

    Functionality is all there

--- BasicCard.js
+++ BasicCard.js
@@ -1,29 +1,56 @@
 var inquirer = require("inquirer");
+var BasicCard = require("./module.js");
+
 
 //Creating our global variables
 var count = 0;
 var cards = [];
 
-//Creating a new constructor function for BasicCard
-function BasicCard (front, back){
-	this.front = front;
-	this.back = back;
-};
+
 
 //Creating a new variable for a new card. This has the first question and first answer
-var card1 = new BasicCard("Question question question", "Answer answer answer");
-var card2 = new BasicCard("Question question question TWO", "Answer answer answer TWO");
+var card1 = new BasicCard("Who invented peanut butter?", "George Washington Carver");
+var card2 = new BasicCard("Question question question TWO", "Answer");
+var card3 = new BasicCard("What is the best way to code?", "Janky");
+var card4 = new BasicCard("What is my favorite animal?", "Sloths");
 
-//Pushing input from user into the cards array. 
-cards.push(cards1);
-cards.push(cards2);
+ 
+cards.push(card1);
+cards.push(card2);
+cards.push(card3);
+cards.push(card4);
 
-function askBasic(){
-	console.log("We're going to ask you 10 questions! See how many you can get right!");
-	if (count < 10){
+
+var askBasic = function(){
+	if (count < 4){
 		inquirer.prompt([
 		{
+			type: "input",
+			message: cards[count].front,
+			name: "response"
 
 		}
-	}
+		]).then(function(answers) {
+			if(answers.response == cards[count].back){
+			console.log("You got the question right!");
+		}
+			else{
+				console.log("You got the question wrong!");
+			}
+		count++;
+		askBasic();
+	});
+			
+
 }
+}
+
+askBasic();
+
+
+
+
+
+
+
+
---
+++ module.js
@@ -0,0 +1,9 @@
+
+
+//Creating a new constructor function for BasicCard
+function BasicCard (front, back){
+	this.front = front;
+	this.back = back;
+};
+
+module.exports = BasicCard;
